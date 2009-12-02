68b81feca3511353a40725dc960b0b995ced6f87
Eric Davis <edavis@littlestreamsoftware.com>
2009-12-02 10:56:43 -0800

    [#3410] Changed the core permissions

--- init.rb
+++ init.rb
@@ -10,16 +10,5 @@ Redmine::Plugin.register :redmine_subtasks do
   version '0.0.1'
   requires_redmine :version_or_higher => '0.8.0'
 
-  # remapping permissions
-  Redmine::AccessControl.permissions.delete_if do |p|
-    p.name == :manage_issue_relations
-  end
-  project_module :issue_tracking do |map|
-    map.permission :manage_issue_relations, {
-      :issue_relations => [:new, :destroy],
-      :issues => [:add_subissue]
-    }
-  end
-
 end
 
